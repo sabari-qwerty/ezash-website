5f09c4f7abd183e736cd984b8327782616ded751
sabari-qwerty <120260499+sabari-qwerty@users.noreply.github.com>
2023-12-25 12:09:21 +0530

    FIEX

--- website/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { HomePage } from "../pages/HomePage";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-]);
